fec0c02d1250ef420ba98a7de830514343b267f6
DRAGQ <1jozef8@gmail.com>
2025-07-19 14:17:38 +0200

    first commit

---
+++ src/Country.java
@@ -0,0 +1,22 @@
+public class Country {
+    private String name;
+    private String capitalCity;
+    private double population;
+
+    Country(String name, String capitalCity, double population) {
+        this.name = name;
+        this.capitalCity = capitalCity;
+        this.population = population;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCapitalCity() {
+        return this.capitalCity;
+    }
+
+    public double getPopulation() {
+        return this.population;
+    }
+}
